84d665f8fa3c0859090f7d14fab7df6a26798b69
NaveenSigirala <101165307+NaveenSigirala@users.noreply.github.com>
2023-03-04 10:32:33 +0530

    Create 2496. Maximum Value of a String in an Array.py

---
+++ 2496. Maximum Value of a String in an Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumValue(self, strs: List[str]) -> int:
+        mx = 0
+
+        for s in strs:
+            if s.isdigit(): mx = max(mx,int(s))
+            else          : mx = max(mx,len(s))
+
+        return mx
